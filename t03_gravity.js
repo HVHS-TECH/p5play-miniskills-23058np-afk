686618c1a4c3fd9973304dfa1429aa9b18fe58ec
Nicholas Phang <23058np@hvhs.school.nz>
2026-02-10 12:30:08 +1300

    things happen

--- t03_gravity.js
+++ t03_gravity.js
@@ -9,14 +9,32 @@
 /*******************************************************/
 
 function setup() {
-	
-	cnv = new Canvas(windowWidth, windowHeight);
-	world.gravity.y = 10;
-	rectangleSprite = new Sprite(windowWidth/2, 0, 100, 160, 'd'); //'d' lets it move dynamically, ie. is affected by gravity AND sprites. use 'k' for just code.
-	rectangleSprite.color = 'red';
-	rectangleSprite.rotationSpeed = -1;
+	try {
+		cnv = new Canvas(windowWidth, windowHeight);
+		world.gravity.y = 10;
+		world.gravity.x = 2
+		
+		rectangleSprite = new Sprite(windowWidth/2, 0, 80, 120, 'd'); //'d' lets it move dynamically, ie. is affected by gravity AND sprites. use 'k' for just code.
+		rectangleSprite.color = 'red';
+		rectangleSprite.rotationSpeed = -1;
+		rectangleSprite.vel.y = -3;
+		
 
-	console.log("setup: it's in the stratosphere!");
+		cubeSprite = new Sprite(windowWidth/2 + 300, 0, 50, 50, 'n')
+		cubeSprite.color = 'blue';
+		cubeSprite.rotationSpeed = -5;
+		cubeSprite.vel.x = -5;
+
+		circleSprite = new Sprite(windowWidth/2 - 300, 100, 80, 'k')
+		circleSprite.color = 'green';
+		circleSprite.rotationSpeed = 13;
+		circleSprite.vel.x = 6;
+
+		console.log("red goes up, green goes right, blue goes left, and they are all pulled down. exept green. and blue. blue figured out the meaning to life.");
+	} catch (error) {
+		console.log("something is wrong")
+	}
+	
 }
 /*******************************************************/
 // draw()
