52d5c12630ae352ffac4586b1c37d432611144d3
Nicholas Phang <23058np@hvhs.school.nz>
2026-02-10 12:12:11 +1300

    as above

--- t03_gravity.js
+++ t03_gravity.js
@@ -7,16 +7,22 @@
 /*******************************************************/
 // setup()
 /*******************************************************/
-function setup() {
-	console.log("setup: ");
 
-}
+function setup() {
 	
+	cnv = new Canvas(windowWidth, windowHeight);
+	world.gravity.y = 10;
+	rectangleSprite = new Sprite(windowWidth/2, 0, 100, 160, 'd'); //'d' lets it move dynamically, ie. is affected by gravity AND sprites. use 'k' for just code.
+	rectangleSprite.color = 'red';
+	rectangleSprite.rotationSpeed = -1;
+
+	console.log("setup: it's in the stratosphere!");
+}
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-	
+	background("black")
 }
 
 /*******************************************************/
