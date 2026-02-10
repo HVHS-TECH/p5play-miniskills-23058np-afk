b362afb537c666f220e063f6394de571b6b07bd5
Nicholas Phang <23058np@hvhs.school.nz>
2026-02-10 23:22:14 +1300

    task 4, done! also added a thing which show sthe cursor position, for quality of life.

--- t04_collision.js
+++ t04_collision.js
@@ -8,15 +8,47 @@
 // setup()
 /*******************************************************/
 function setup() {
-	console.log("setup: ");
+	
+	cnv = new Canvas(windowWidth, windowHeight);
+	world.gravity.y = 10;
+	try {
+		mySprite = new Sprite(windowWidth/2, 0, 80, 120, 's'); 
+		mySprite.color = 'RGB(31, 112, 256)';
+		mySprite.rotationSpeed = 2;
+		mySprite.vel.x = 2;
+		mySprite.drag = 2
+		
+		//platform_1
+		platform_1 = new Sprite(400, 200, 100, 10, 'd');
+		platform_1.rotation = 25
+		platform_1.bounciness = 1.5
+		platform_1.vel.x=2
+		
 
+		//platform_2
+		platform_2 = new Sprite(820, 240, 100, 10, 'k');
+		platform_2.rotation = -30
+		platform_2.bounciness = 1
+		platform_2.friction = 200
+		//platform_3
+		platform_3 = new Sprite(600, 600, 100, 10, 'k');
+		platform_3.bounciness = 2
+		console.log("setup:sucess ");
+	} catch (error) {
+		console.log("setup:fail ");
+
+	}
+	
 }
 	
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-	
+	textSize(32)
+	background("RGB(256,256,256)");	
+	text(`x: ${mouseX} y:${mouseY}`,50, 50,);//so i can easily find where to put things
+
 }
 
 /*******************************************************/
