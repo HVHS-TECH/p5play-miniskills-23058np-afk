266ed95e55491b9842b7d8815ee2e2d08b8458f9
Nicholas Phang <23058np@hvhs.school.nz>
2026-02-11 14:29:44 +1300

    fixed the thing basing off a position instead of \the window

--- t03_gravity.js
+++ t03_gravity.js
@@ -14,22 +14,23 @@ function setup() {
 		world.gravity.y = 10;
 		world.gravity.x = 2
 		
-		rectangleSprite = new Sprite(windowWidth/2, 0, 80, 120, 'd'); //'d' lets it move dynamically, ie. is affected by gravity AND sprites. use 'k' for just code.
+		rectangleSprite = new Sprite(windowWidth*0.5, windowHeight, 80, 120, 'd'); //'d' lets it move dynamically, ie. is affected by gravity AND sprites. use 'k' for just code.
 		rectangleSprite.color = 'red';
 		rectangleSprite.rotationSpeed = -1;
 		rectangleSprite.vel.y = -3;
 		
 
-		cubeSprite = new Sprite(windowWidth/2 + 300, 0, 50, 50, 'n')
+		cubeSprite = new Sprite(windowWidth*0.5, windowHeight, 0, 50, 50, 'n')
 		cubeSprite.color = 'blue';
 		cubeSprite.rotationSpeed = -5;
-		cubeSprite.vel.x = -5;
+		
 
-		circleSprite = new Sprite(windowWidth/2 - 300, 100, 80, 'k')
+		circleSprite = new Sprite(windowWidth*0.5, windowHeight, 100, 80, 'k')
 		circleSprite.color = 'green';
 		circleSprite.rotationSpeed = 13;
 		circleSprite.vel.x = 6;
 
+		console.log(windowHeight);
 		console.log("red goes up, green goes right, blue goes left, and they are all pulled down. exept green. and blue. blue figured out the meaning to life.");
 	} catch (error) {
 		console.log("something is wrong")
