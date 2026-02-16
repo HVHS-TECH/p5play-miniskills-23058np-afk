74fc756e7ac5ed046a296c1ca34a5bc6f2bce6d8
Nicholas Phang <23058np@hvhs.school.nz>
2026-02-16 13:01:55 +1300

    gave a ball that  moves with the timer to t23

--- t23_text.js
+++ t23_text.js
@@ -8,35 +8,43 @@
 // setup()
 /*******************************************************/
 function setup() {
-	
-	world.gravity.y = 0
-	cnvPadding = 12 
-	cnv = new Canvas(windowWidth-cnvPadding, windowHeight-cnvPadding);
-	try {
-		ballOne = new Sprite(width/2, height/2, 50, 'd');
-		ballOne.drag = 10
-		console.log("setup: success ");
-	} catch (error) {
-		console.log("setup: fail ");
-	}
-}
-	
+        textAlign(CENTER, CENTER);
+        world.gravity.y = 0
+        cnvPadding = 12 
+        cnv = new Canvas(windowWidth-cnvPadding, windowHeight-cnvPadding);
+        try {
+                ballOne = new Sprite(width/2, height/2, 50, 'd');
+                ballOne.drag = 10
+                console.log("setup: success ");
+        } catch (error) {
+                console.log("setup: fail ");
+            }
+        }
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-	background("grey");
-    text("Hello World", 50, 50) 
-    var name = "Mr Bob";
-    text("Hello "+name, 50, 100);
-    //movement
-	right = Math.min(kb.pressing('right'),1);
-	left = Math.min(kb.pressing('left'),1);
-	up = Math.min(kb.pressing('up'),1);
-	down = Math.min(kb.pressing('down'),1);
+        background("grey");
+        textSize(64);
+        
+        //movement
+        right = Math.min(kb.pressing('right'),1);
+        left = Math.min(kb.pressing('left'),1);
+        up = Math.min(kb.pressing('up'),1);
+        down = Math.min(kb.pressing('down'),1);
 
-	ballOne.vel.y += down - up
-	ballOne.vel.x += right - left
+        ballOne.vel.y += down - up
+        ballOne.vel.x += right - left
+
+        //time in seconds
+        let seconds=nf(millis() / 1000,1,0);
+        // degrees in seconds time 180
+        let sine = 180*nf(millis() / 1000,1,10);
+        // returns -1=<x=<1 every second
+        let x = sin(sine);
+        circle(250*x+width/2,height/2,50);
+        text(seconds,width/2,height*0.8);
+        // the ball and the timer are off by varying amounts of 2 frames. I will blame p5 for that.
 }
 /*******************************************************/
 //  END OF APP
