c891ceb29042149e4540fd5c2a2d76de15640a7a
Nicholas Phang <23058np@hvhs.school.nz>
2026-02-16 15:37:14 +1300

    t23 done

--- t23_text.js
+++ t23_text.js
@@ -8,6 +8,7 @@
 // setup()
 /*******************************************************/
 function setup() {
+
         textAlign(CENTER, CENTER);
         world.gravity.y = 0
         cnvPadding = 12 
@@ -25,16 +26,18 @@ function setup() {
 /*******************************************************/
 function draw() {
         background("grey");
-        textSize(64);
-        
+        textSize(128);
+        stroke(0);
+        strokeWeight(2);
+        fill('limegreen');
         //movement
         right = Math.min(kb.pressing('right'),1);
         left = Math.min(kb.pressing('left'),1);
         up = Math.min(kb.pressing('up'),1);
         down = Math.min(kb.pressing('down'),1);
 
-        ballOne.vel.y += down - up
-        ballOne.vel.x += right - left
+        ballOne.vel.y += down - up;
+        ballOne.vel.x += right - left;
 
         //time in seconds
         let seconds=nf(millis() / 1000,1,0);
@@ -43,7 +46,8 @@ function draw() {
         // returns -1=<x=<1 every second
         let x = sin(sine);
         circle(250*x+width/2,height/2,50);
-        text(seconds,width/2,height*0.8);
+        text(seconds,width/2,height*0.8,);
+        
         // the ball and the timer are off by varying amounts of 2 frames. I will blame p5 for that.
 }
 /*******************************************************/
