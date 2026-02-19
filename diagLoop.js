2cbfc8a6ab90eeb87e870a2566b55dfb1c274d9c
Nicholas Phang <23058np@hvhs.school.nz>
2026-02-20 09:19:44 +1300

    did most of the loops

---
+++ diagLoop.js
@@ -0,0 +1,14 @@
+function setup(){
+    cnv = new Canvas(900,800)
+    for (var i = 0; i < 11; i++) {
+    var block = new Sprite(i*80 + 40, i*70+40, 50, 50);
+    block.color = color(255, 0, 0); // Red color
+    console.log (i)
+    }
+}
+
+function draw(){
+    background("cyan")
+    
+
+}
