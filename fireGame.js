8f34660095284d411712d86de4b44ca6e9b92df7
Nicholas Phang <23058np@hvhs.school.nz>
2026-02-23 11:05:18 +1300

    started on the fire game

---
+++ fireGame.js
@@ -0,0 +1,16 @@
+
+function setup() {
+        canvas = new Canvas(500,500);
+        
+}
+
+function draw() {
+        background("grey");
+}
+/*
+https://www.youtube.com/watch?v=AdmGHwvgaVs
+https://docs.google.com/document/d/1WdJHmXq72qoPJmYlfV4K6h4ceAhk6pxniKo1zHxzQpo/edit?tab=t.0
+https://github.com/HVHS-TECH/programming-project-23058np-afk?tab=readme-ov-file
+https://docs.google.com/document/d/1Uevcw4lIK0amCPF05YiVLGuc4D_j6qW-bViowcld2kc/edit?tab=t.0
+https://www.youtube.com/watch?v=lgck-txzp9o
+*/
